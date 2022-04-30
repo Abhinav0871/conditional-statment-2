8281341c66c4fac53a30394a76acfd69c42dabd9
Abhinav0871 <100013024+Abhinav0871@users.noreply.github.com>
2022-04-30 16:48:22 +0530

    Update switch case.c

--- switch case.c
+++ switch case.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int choice, b, f, p, s, Burger, French, Pizza, Sandwiches;
+    int choice, b, f, p, s,pa, Burger, French, Pizza, Sandwiches, pasta;
     printf("1.Burger\n2.French Fries\n3.Pizza\n4.Sandwiches\n");
     printf("Please select your choice from 1,2,3,4\n");
     scanf("%d", &choice);
@@ -12,30 +12,37 @@ int main()
         printf("You selected Burger.\n");
         printf("Please select quantity : ");
         scanf("%d", &b);
-        Burger = 200 * b;
+        Burger = 129 * b;
         printf("Your bill is : %d", Burger);
         break;
     case 2:
         printf("You selected French Fries. \n");
         printf("Please select quantity : ");
         scanf("%d", &f);
-        French = 50 * f;
+        French = 99 * f;
         printf("Your bill is %d", French);
         break;
     case 3:
         printf("You selected Pizza.\n");
         printf("Please select quantity : ");
         scanf("%d", &p);
-        Pizza = 500 * p;
+        Pizza = 239 * p;
         printf("Your bill is %d", Pizza);
         break;
     case 4:
         printf("You selected Sandwiches.\n");
         printf("Please select quantity : ");
         scanf("%d", &s);
-        Sandwiches = 150 * s;
+        Sandwiches = 149 * s;
         printf("Your bill is %d", Sandwiches);
         break;
+    case 5:
+        printf("you selected pasta.\n");
+        printf("please select quantity : ");
+        scanf( "%d", &pa);
+        pasta = 179 * pa;
+        printf("your bill is %d", pasta);
+        break;    
     default:
         printf("Invalid choice");
         break;
